0a4f09209009063dc98d7b689613a7f5dec7278b
Shivam Gupta <shivamaggarwal767@gmail.com>
2023-06-08 17:07:02 +0530

    added super method

--- src/main/java/com/driver/B.java
+++ src/main/java/com/driver/B.java
@@ -1,7 +1,9 @@
 package com.driver;
 
 public class B extends A{
-
+    public String callSuper(){
+        return super.meth();
+    }
     @Override
     public String meth(){
         return "Method is overridden in Extendend class B";
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         B obj = new B();
-        String res1 = obj.meth();
-        System.out.print(res1);
+        String res1 = obj.callSuper();
+        System.out.println(res1);
+        String res2 = obj.meth();
+        System.out.println(res2);
     }
 }
