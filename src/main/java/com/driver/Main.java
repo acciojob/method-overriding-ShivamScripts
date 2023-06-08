347528193225a9238f04519afbddea5a713b5e14
Shivam Gupta <shivamaggarwal767@gmail.com>
2023-06-08 17:14:20 +0530

    error check

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,13 +3,9 @@ package com.driver;
 public class Main {
     public static void main(String[] args){
         B obj = new B();
+        obj.meth();
 
-        // Calling the method from class A
-        String result1 = obj.meth();
-        System.out.println(result1);
-
-        // Calling the overridden method from class B
-        String result2 = obj.meth();
-        System.out.println(result2);
+        B obj1 = new B();
+        obj1.meth();
     }
 }
